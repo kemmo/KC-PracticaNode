ea3451c488f49b89d86774e64a39eb0004d52af5
Jorge Vinaches <jorge.vinaches@gruposantander.com>
2017-12-10 11:34:50 +0100

    Create mongoose db connector

--- nodepop/app.js
+++ nodepop/app.js
@@ -10,6 +10,9 @@ var users = require('./routes/users');
 
 var app = express();
 
+//Cargamos el conector a BBDD
+require('./lib/connectMongoose');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
