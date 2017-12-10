1bb1417a0ce14537ca484bb1cda58cee1902c8df
Jorge Vinaches <jorge.vinaches@gruposantander.com>
2017-12-10 23:50:23 +0100

    Remove Anuncio saving test

--- nodepop/routes/index.js
+++ nodepop/routes/index.js
@@ -1,31 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const Anuncio = require('../models/Anuncio');
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-
-  //TODO: Remove following test
-  //Test saving Anuncio
-  const anuncio = new Anuncio({
-    nombre: "Bici",
-    venta: true,
-    precio: 200.15,
-    foto: "images/anuncios/photo.jpg",
-    tags: ["lifestyle"]
-  });
-
-  anuncio.save((err, anuncioGuardado) => {
-    if (err) {
-      next(err);
-      return;
-    }
-
-    res.json({result: anuncioGuardado});
-  });
-  //End test
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
